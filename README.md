fa5c93dfb57dfd7778dcc449785024a8742a3cd8
TomSoerr <tomix2004@gmail.com>
2024-02-13 22:22:34 +0100

    Finish product modal

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Disclaimer for Material Symbols Font: The "Material Symbols" font in this projec
 Disclaimer for Federo Font: The "Federo" font in this project is subject to the [SIL Open Font License, Version 1.1]( https://openfontlicense.org) and was created by Olexa M. Volochay
 
 Disclaimer Noto Sans Font: The "Noto Sans" font in this project is subject to the [SIL Open Font License, Version 1.1]( https://openfontlicense.org)
+
+### Bilder 
+
+Hero Image: Photo by <a href="https://unsplash.com/@ksheehan77?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Kieran Sheehan</a> on <a href="https://unsplash.com/photos/green-leafed-trees-during-daytime-qu2nE8R48Kw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
--- website/js/modules/modal.js
+++ website/js/modules/modal.js
@@ -5,35 +5,50 @@ import picture from './picture.js';
 const _ = Helper.create;
 
 export default function modal({ name, img, price, description, variant }) {
+  const descriptionArray = description.split('<br>');
+
   return _('dialog', { class: 'tst-modal' }, [
-    picture({
-      src: img.src,
-      alt: img.alt,
-    }),
     _('div', null, [
-      _('h2', null, [name]),
-      _('p', { class: 'tst-modal-price' }, [price]),
-      _('p', { class: 'tst-modal-description' }, [description]),
-      variant
-        ? _('ul', null, [
-            ...variant.reduce((prev, curr) => {
-              prev.push(_('li', null, [curr]));
-              return prev;
-            }, []),
-          ])
-        : null,
-      _('form', { method: 'dialog' }, [
+      picture({
+        src: img.src,
+        alt: img.alt,
+      }),
+      _('div', null, [
+        _('h2', { text: name }),
+        _('p', { class: 'tst-product-price', text: price }),
+        ...descriptionArray.reduce((prev, curr) => {
+          prev.push(_('p', { text: curr }));
+          return prev;
+        }, []),
+
+        variant ? _('h3', { text: 'Varianten' }) : '',
+        variant
+          ? _('ul', null, [
+              ...variant.reduce((prev, curr) => {
+                prev.push(_('li', null, [curr]));
+                return prev;
+              }, []),
+            ])
+          : '',
         button({
-          type: 'submit',
-          text: 'Close',
+          type: 'link',
+          text: 'Kontaktieren',
+          href: 'kontakt.html',
           color: 'secondary',
-          event: {
-            type: 'click',
-            listener: (e) => {
-              document.body.style.overflow = 'auto';
-            },
-          },
         }),
+        _('form', { method: 'dialog' }, [
+          button({
+            type: 'submit',
+            text: 'Zurück',
+            color: 'secondary',
+            event: {
+              type: 'click',
+              listener: (e) => {
+                document.body.style.overflow = 'auto';
+              },
+            },
+          }),
+        ]),
       ]),
     ]),
   ]);
