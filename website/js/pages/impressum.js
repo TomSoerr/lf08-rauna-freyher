005c28f5167e0db666db9edac9c22c9aab68073d
TomSoerr <tomix2004@gmail.com>
2024-06-17 16:20:08 +0200

    Update impressum.js

--- website/js/pages/impressum.js
+++ website/js/pages/impressum.js
@@ -6,7 +6,11 @@ import p from '../modules/paragraph.js';
 
 function load() {
   document.body.append(nav(), main(
-    section(p('Angaben gemäß §5 TMG <br> Rauna Freyher <br>Wiesengrund 125 <br>22534 Hamburg <br> <br>Kontakt <br>Telefon (0 40) 33 30 36- 0  <br>Telefax (0 40) 33 30 36-100 <br>E-Mail: info@raunafreyher.de <br>www.raunafreyher-naturkosmetik.de <br>Geschäftsführerin: Rauna Freyher <br> <br>Verantwortlich für diese Seiten: Rauna Freyer')),
+    section([
+      'Keine kommerzielle Website, nur für Bildungszwecke.',
+      _('br'),
+      _('a', { href: 'https://tomsoerr.github.io/#/impressum', text: "Impressum" })
+    ]),
   ), footer());
 }
 
